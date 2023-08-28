444a5cbfc8ef0394ccf20731a353867ac24e2056
Abdul Warish <er.abdulwaris@gmail.com>
2023-08-28 21:01:30 +0300

    Initial commit

---
+++ src/main/java/com/atech/eshop/Eshop/controller/AuthController.java
@@ -0,0 +1,12 @@
+package com.atech.eshop.Eshop.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AuthController {
+    @GetMapping("/")
+    public String register(){
+        return "Welcome to Eshop";
+    }
+}
---
+++ src/test/java/com/atech/eshop/Eshop/EshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atech.eshop.Eshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
